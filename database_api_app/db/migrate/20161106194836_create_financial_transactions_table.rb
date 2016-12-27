72a429e6d6b522da84a50db024bb62c5e55a77a5
Asif Ahmed <asif.h.ahmed@gmail.com>
2016-12-27 11:46:42 -0800

    Add Migrations to DB Api

---
+++ database_api_app/db/migrate/20161106194836_create_financial_transactions_table.rb
@@ -0,0 +1,18 @@
+class CreateFinancialTransactionsTable < ActiveRecord::Migration[5.0]
+  def change
+    create_table :financial_transactions do |t|
+      t.string :plaid_id, index: true
+      t.string :plaid_name, index: true
+      t.string :spreadsheet_name, index: true
+      t.string :category, index: true
+      t.string :source
+      t.decimal :amount, :precision => 8, :scale => 2
+      t.datetime :transacted_at, index: true
+
+      t.boolean :hidden, :default => false
+      t.boolean :uploaded, :default => false
+
+      t.timestamps
+    end
+  end
+end
