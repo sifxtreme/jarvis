c8fd4dac64717f7d564ce4a7f525a3dae5a3336d
Asif Ahmed <asif.h.ahmed@gmail.com>
2017-02-22 21:10:25 -0800

    Better Error Handling for CLI

--- cli.rb
+++ cli.rb
@@ -1,21 +1,32 @@
 require_relative './app/lib/plaid/api'
 require_relative './app/lib/google_drive/api'
 require_relative './app/lib/southwest/search'
+require_relative './app/lib/analysis/finances'
 
 plaid = Plaid::Api.new
 spreadsheet = FinanceSpreadsheet::Api.new
 southwest = Southwest.new
+analysis = Analysis::Finances.new
 
 arg = ARGV[0]
 
-if arg == 'sync_to_drive'
-  plaid.sync_all
-  spreadsheet.sync_to_drive
-
-elsif arg == 'sync_from_drive'
-  spreadsheet.sync_from_drive
-
-elsif arg == 'southwest'
-  southwest.runner
+begin
   
+  if arg == 'sync_to_drive'
+    plaid.sync_all
+    spreadsheet.sync_to_drive
+
+  elsif arg == 'sync_from_drive'
+    spreadsheet.sync_from_drive
+
+  elsif arg == 'southwest'
+    southwest.runner
+
+  elsif arg == 'email'
+    analysis.email_report
+
+  end
+
+rescue StandardError => e
+  puts e.message
 end
