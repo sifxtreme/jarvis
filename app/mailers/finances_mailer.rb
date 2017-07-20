fe5fb648407ad436a7860c917061fc3155df84c8
Asif Ahmed <asifahmed2011@gmail.com>
2017-07-20 12:02:36 -0700

    Uncomment Wife

--- app/mailers/finances_mailer.rb
+++ app/mailers/finances_mailer.rb
@@ -5,7 +5,7 @@ class FinancesMailer < ApplicationMailer
     @all_categories = all_categories
     @uncategorized_records = uncategorized_records
     mail(to: 'asif.h.ahmed@gmail.com',
-      # cc: 'hsayyeda@gmail.com',
+      cc: 'hsayyeda@gmail.com',
       subject: "Finances - #{DateTime.now.strftime('%m/%d/%Y')}")
   end
 
