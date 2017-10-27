ec27ad5bfc969cf2ec6fa025a32cfa901f30e853
Asif Ahmed <asif.h.ahmed@gmail.com>
2017-10-27 11:05:53 -0700

    Remove HTTP Basic Auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,3 @@
 class ApplicationController < ActionController::API
-  include ActionController::HttpAuthentication::Basic::ControllerMethods
-  http_basic_authenticate_with name: ENV['JARVIS_USER'], password: ENV['JARVIS_PASS']
+  
 end
