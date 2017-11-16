8e134de7ecbc70d068c29fca8576a8c9ba2a1b80
Asif Ahmed <asif.h.ahmed@gmail.com>
2017-11-16 16:03:48 +0000

    Remove CORs

--- config/application.rb
+++ config/application.rb
@@ -42,13 +42,5 @@ module JarvisApi
     }
 
     config.skylight.environments += ["development"]
-
-    config.middleware.insert_before 0, Rack::Cors do
-      allow do
-        origins 'http://localhost:300'
-        resource '*', :headers => :any, :methods => [:get, :post, :options]
-      end
-    end
-
   end
 end
