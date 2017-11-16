a28f71216a417d3bba355b5efb2d308f2f6f14d9
Asif Ahmed <asif.h.ahmed@gmail.com>
2017-11-16 15:19:28 +0000

    Bugs

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -10,6 +10,6 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
     origins '*'
     resource '*',
       headers: :any,
-      methods: [:get, :post, :put]
+      methods: [:get, :post, :put, :patch, :options, :head]
   end
 end
