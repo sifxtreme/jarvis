9e586be252415310c2ed348c5714716377d9a7cc
Asif Ahmed <asif.h.ahmed@gmail.com>
2017-11-16 16:01:44 +0000

    CORs

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,10 @@
 class HomeController < ApplicationController
 
   def index
+headers['Access-Control-Allow-Origin'] = '*'
+headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
+headers['Access-Control-Request-Method'] = '*'
+headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
     render json: {app_name: 'jarvis', environment: Rails.env}
   end
   
