72fbb2ce6d1cdf97ab73f8668aceee690266a83c
Asif Ahmed <asif.h.ahmed@gmail.com>
2017-11-16 15:34:27 +0000

    More CORs

--- config/application.rb
+++ config/application.rb
@@ -46,10 +46,9 @@ module JarvisApi
     config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins '*'
-        resource '*',
-          headers: :any,
-          methods: [:get, :post, :put, :patch, :options, :head]
+        resource '*', :headers => :any, :methods => [:get, :post, :options]
       end
     end
+    
   end
 end
