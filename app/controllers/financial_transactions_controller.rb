23543e73fbf12ab8a9201399c7b9dbeb8eb901df
Asif Ahmed <asif.h.ahmed@gmail.com>
2017-12-08 17:17:59 -0800

    Standardize Financial Transaction Controller

---
+++ app/controllers/financial_transactions_controller.rb
@@ -0,0 +1,53 @@
+class FinancialTransactionsController < ApplicationController
+
+  def index
+    db_query = FinancialTransaction.all
+    
+    year = params[:year]
+    month = params[:month]
+    query = params[:query]
+    db_query = db_query.where("YEAR(transacted_at) = ?", year) if year
+    db_query = db_query.where("MONTH(transacted_at) = ?", month) if month
+    db_query = db_query.where("category like ? or merchant_name like ? or plaid_name like ?", "%#{query}%", "%#{query}%", "%#{query}%") if query
+    db_query = db_query.limit(10) 
+    results = db_query.order('transacted_at DESC')
+    total = results.inject(0){|sum,x| sum + x.amount }
+    
+    render json: {total: total, results: results.map }
+  end
+
+  def create
+    f = FinancialTransaction.new
+    data = JSON.parse(request.body.read)
+    f.plaid_id = data["plaid_id"]
+    f.plaid_name = data["merchant_name"]
+    f.merchant_name = data["merchant_name"]
+    f.category = data["category"]
+    f.amount = data["amount"]
+    date = Date.parse(data["transacted_at"]) rescue Date.today
+    f.transacted_at = date
+    f.source = data["source"]
+    f.hidden = data["hidden"]
+    f.reviewed = data["reviewed"]
+    f.save!
+
+    render json: f
+  end
+
+  def update
+    f = FinancialTransaction.find(params[:id])
+    data = JSON.parse(request.body.read)
+    f.merchant_name = data["merchant_name"]
+    f.category = data["category"]
+    f.amount = data["amount"]
+    date = Date.parse(data["transacted_at"]) rescue Date.today
+    f.transacted_at = date
+    f.source = data["source"]
+    f.hidden = data["hidden"]
+    f.reviewed = data["reviewed"]
+    f.save!
+
+    render json: f
+  end
+  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,11 @@
 Rails.application.routes.draw do
   root to: 'home#index'
-  
+
   get 'plaid/balances', to: 'plaid#balances'
   get 'plaid/transactions/:bank_id', to: 'plaid#transactions'
   
-
-  get 'finances/rolling_budget', to: 'finances#rolling_budget'
-  get 'finances/transactions', to: 'finances#transactions'
-  get 'finances/balances', to: 'finances#balances'
-
+  resources :financial_transactions, only: [:index, :create, :update]
+  
   mount Resque::Server, :at => "resque"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
